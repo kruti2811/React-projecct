431f4517550669bf0d8d95a94ec5fb43f2e7e6af
Dimpal karena <dimpalkarena2@gmail.com>
2025-04-01 09:56:57 +0530

    changes css in navbar

--- the voice of geeta/src/components/Navbar.jsx
+++ the voice of geeta/src/components/Navbar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom"; // ✅ Import Link for Navigation
+import { Link } from "react-router-dom";
 import "../assets/css/navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="container">
-        <div className="logo">
-          <img src="/images/Rose.png" alt="Voice of Geeta" />
-          <span>Voice of Geeta</span>
-        </div>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li> {/* ✅ Updated from Home to Dashboard */}
-          <li><Link to="/adhyay">Adhyay</Link></li>
-          <li><Link to="/vedas">Vedas</Link></li>
-          <li><Link to="/teachings">Teachings</Link></li>
-        </ul>
-        <div className="profile-icon">
-          <img src="/images/profile.png" alt="Profile" />
-        </div>
+      <div className="logo">
+        <img src="/images/Rose.png" alt="Logo" />
+        <span>Voice of Geeta</span>
+      </div>
+
+      {/* Hamburger Menu Checkbox */}
+      <input type="checkbox" id="menu-toggle" className="menu-checkbox" />
+      <label htmlFor="menu-toggle" className="menu-icon">☰</label>
+
+      {/* Navigation Links */}
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/adhyay">Adhyay</Link></li>
+        <li><Link to="/vedas">Vedas</Link></li>
+        <li><Link to="/teachings">Teachings</Link></li>
+      </ul>
+      
+      <div className="profile-icon">
+        <img src="/images/profile.png" alt="Profile" />
       </div>
     </nav>
   );
